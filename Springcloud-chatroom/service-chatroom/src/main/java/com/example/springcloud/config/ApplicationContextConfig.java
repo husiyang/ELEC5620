10cd09dd5b130df8563ab9141a9e46b907b3bd1f
husiyang <2803149178@qq.com>
2022-10-16 22:51:45 +1100

    delete microservice

--- Springcloud-chatroom/service-chatroom/src/main/java/com/example/springcloud/config/ApplicationContextConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.springcloud.config;
-
-import org.springframework.boot.SpringBootConfiguration;
-
-@SpringBootConfiguration
-public class ApplicationContextConfig {
-
-
-}
--- Springcloud-chatroom/service-chatroom/src/main/java/com/example/springcloud/controller/UserController.java
+++
@@ -1,4 +0,0 @@
-package com.example.springcloud.controller;
-
-public class UserController {
-}
